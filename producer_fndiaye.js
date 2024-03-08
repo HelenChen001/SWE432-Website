73086fc08757b55c8e9d2b3cfbeba437d7012e06
ndiayef <fndiaye17@outlook.com>
2024-03-08 01:44:03 -0500

    Implemented Producer Page JS
    continued implementation for producer pages, edit and play order

--- producer_fndiaye.js
+++ producer_fndiaye.js
@@ -1,9 +1,9 @@
 window.onload = function () {
-  console.log("Window.onload: Page as fully loaded!");
+  console.log("Window.onload: Page is fully loaded!");
 
-  setTimeout(function () {
-    console.log("Delayed Message for 2 sections");
-  }, 2000);
+  //   setTimeout(function () {
+  //     console.log("Delayed Message for 2 sections");
+  //   }, 2000);
 
   class Story {
     constructor(title, author, genre, duration) {
@@ -119,5 +119,176 @@ window.onload = function () {
     );
   } else {
     console.log(story_two + " airs from " + story_two.time_slot);
+  }
+
+  // changing the color of the producer tab in the navbar
+  let producer_tab = document.getElementById("producer-nav-tab");
+  producer_tab.style.background =
+    "linear-gradient(to bottom, #5f4bb6, #f7f5fb)";
+  producer_tab.style.padding = "5px";
+
+  // FORM VALIDATION
+  const saveButton = document.getElementById("save-button");
+
+  // getting all the dropdowns
+  const dropdowns = document.querySelectorAll(".playlist-grid .time_options");
+  let formChanged = false;
+
+  // getting the initial state of the form
+  let initialDropdownValues = [];
+  dropdowns.forEach(function (dropdown) {
+    initialDropdownValues.push(dropdown.value);
+  });
+
+  dropdowns.forEach(function (dropdown, index) {
+    // checking for changes
+    dropdown.addEventListener("change", function () {
+      console.log("here");
+      formChanged = true;
+    });
+  });
+
+  if (saveButton) {
+    // checking if the save button was clicked
+    saveButton.addEventListener("click", function (event) {
+      event.preventDefault();
+      // if the form didnt change
+      if (!formChanged) {
+        alert("No Changes to Playlist");
+        return;
+      }
+      // add changes to database... to be implemented
+      alert("Playlist saved!");
+      window.location.reload();
+    });
+  }
+
+  //SEARCH BAR
+  const search = document.querySelector('.search input[type="text"]');
+
+  if (search) {
+    search.addEventListener("keydown", function (event) {
+      // Check if the pressed key is Enter
+      if (event.key === "Enter") {
+        console.log("Enter key pressed in search input");
+
+        // search function for dj entered
+        performSearch(search.value);
+      }
+    });
+
+    // Function to perform search -- to be implemented
+    function performSearch(query) {
+      alert("Performing search for: " + query);
+      // look for dj in database
+    }
+  }
+
+  const currentPlaylist = document.querySelectorAll(".playlist-item.page2");
+
+  //EDIT PLAYLISTS - ADD PLAYLIST BUTTON
+  const hoverTarget = document.querySelectorAll(".right-item p");
+
+  hoverTarget.forEach(function (new_item) {
+    // event listener for mouseover event
+    new_item.addEventListener("mouseover", function () {
+      // change pointer on over
+      new_item.style.cursor = "pointer";
+    });
+    // event listener for clicking the add to playlist button
+    new_item.addEventListener("click", function () {
+      const parentItem = new_item.closest(".story-search-item");
+
+      // Find the element with class "story-search-results" inside the parent element
+      const searchResults = parentItem.querySelector(".story-search-results");
+      // call function to move the story to the playlist
+      add_to_playlist(searchResults);
+
+      alert("New Story added to Playlist!");
+    });
+  });
+
+  // EDIT PLAYLISTS - ADD STORIES BUTTON
+  // Select the edit-section element
+  const editSection = document.querySelector(".edit-section");
+
+  // Select the first button element within the edit-section element
+  if (editSection) {
+    const buttons = editSection.querySelectorAll("button");
+    const firstButton = buttons[0];
+
+    if (firstButton) {
+      // event listener for mouseover event
+      firstButton.addEventListener("mouseover", function () {
+        // change pointer on over
+        firstButton.style.cursor = "pointer";
+      });
+      // event listener for clicking the add to playlist button
+      firstButton.addEventListener("click", function () {
+        alert(
+          "Add Stories Button Clicked! Search for a story to add to the playlist"
+        );
+      });
+    }
+    // EVENTS FOR DELETING A STORY
+    const secondButton = buttons[1];
+    if (secondButton) {
+      // event listener for mouseover event
+      secondButton.addEventListener("mouseover", function () {
+        // change pointer on over
+        secondButton.style.cursor = "pointer";
+      });
+      // event listener for clicking the add to playlist button
+      secondButton.addEventListener("click", function () {
+        alert(
+          "Delete Stories Button Clicked! Click on an entry in the playlist to delete it "
+        );
+
+        delete_from_playlists();
+      });
+    }
+  }
+
+  function add_to_playlist(searchResults) {
+    let text = searchResults.querySelector("h5").textContent;
+
+    // iterate through the current playlist and look for an empty spot
+
+    for (let i = 0; i < currentPlaylist.length; ++i) {
+      if (currentPlaylist[i].textContent == "") {
+        // need to fix formatting and display
+        currentPlaylist[i].innerHTML += "<p>" + text + i + "</p>";
+        break;
+      }
+    }
+  }
+
+  function delete_from_playlists() {
+    currentPlaylist.forEach(function (item, index) {
+      // if the entry is not empty
+      if (item.textContent != "") {
+        // make the deleting image appear
+        const image = item.querySelector("img");
+
+        image.src = "Images/delete_button.png";
+        image.style.width = "15px";
+        image.style.height = "100%";
+        image.style.visibility = "visible";
+        // event listener for each item
+        image.addEventListener("mouseover", function () {
+          image.style.cursor = "pointer";
+        });
+        // if an item is clicked
+        image.addEventListener("click", function () {
+          const pElement = item.querySelector("p");
+          if (pElement.textContent != "") {
+            pElement.remove();
+            image.style.visibility = "hidden";
+            console.log(item);
+          }
+        });
+      }
+    });
+    console.log(currentPlaylist);
   }
 };
