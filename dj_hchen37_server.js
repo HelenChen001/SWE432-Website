4ae2fbc71106b225ce6e8028dbc7c91dda53a646
HelenChen001 <hchen.8104@gmail.com>
2024-03-23 13:25:22 -0400

    Fixed file locations for express
    Moved files around in order for express to load the front end correctly

--- dj_hchen37_server.js
+++ dj_hchen37_server.js
@@ -1,26 +1,13 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const express = require('express');
+const app = express();
+const path = require("path");
 
-const server = http.createServer((req, res) =>{
-	console.log(req.url, req.method);
-
-	// res.setHeader('Content-Type', 'text/html');
-	
-	// //fs is reading the html file
-	// fs.readFile('./views/dj_hchen37.html', (err, data) =>{
-	// 	if(err){
-	// 		console.log(err);
-	// 	}
-	// 	else{
-	// 		res.write(data);
-	// 	}
-	// 	res.end();
-	// });
-
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.get("/", (req,res) => {
+	res.sendFile(path.join(__dirname, "/views/dj_hchen37.html"));
 });
 
-let port = 8080;
-server.listen(port);
-console.log("Server running at port= " + port);
+
+app.listen(8080, () => {
+	console.log(`Server is running on port 8080`);
+});
