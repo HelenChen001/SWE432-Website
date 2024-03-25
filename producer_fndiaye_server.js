fab7e4164953ef761eb76864468983277a3fbf7d
ndiayef <fndiaye17@outlook.com>
2024-03-25 00:16:45 -0400

    producer server
    began implementing producer server -- to be fixed

---
+++ producer_fndiaye_server.js
@@ -0,0 +1,26 @@
+//importing all modules and packages needed
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const djRoute = require("./routes/djRoute.js");
+const manRoute = require("./routes/managerRoute.js");
+const prodRoute = require("./routes/producerRoute.js");
+
+//Configuring to ejs template engine and routing the js and css files
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+//Routing between the different pages.
+app.use("/", prodRoute);
+app.use("/dj_hchen37.html", djRoute);
+app.use("/manager_anguy38.html", manRoute);
+
+app.get("/", (req, res) => {
+  res.render("producer_fndiaye.ejs");
+});
+
+//Running the port
+app.listen(8080, () => {
+  console.log(`Server is running on port 8080`);
+});
